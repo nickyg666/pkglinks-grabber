b9fb3b7e7cc9f22e07c6f0d407d312f13d764f5a
nickyg666 <93450415+nickyg666@users.noreply.github.com>
2022-04-01 20:22:23 -0400

    rewritten, less code, more efficient
    v 1.1! should work on all platforms and python 3.8/3.9
    dumped feedparser as I don't really need all of that.
    still very obviously knowing nothing about versioning or commit changes.
    still working on parsing 100+ entries, I can get the new url no problem, and with this new approach (requests and json) I should probably be able to get 1000.
    and hey, I'm learning

---
+++ pkglinksresolverv1.1.py
@@ -0,0 +1,29 @@
+import json, requests
+import re, base64
+
+#using JSON instead of RSS, way easier
+filename = input("What should we name the output file? \n Please include .txt if you want extension. (lookin' at you, Windows users) \n .txt file will be stored in directory you ran me from. \n make sure clipboard monitor is enabled in JDownloader2 \n and simply select all text in file and copy, \n JD2 will do the rest. \n You can resolve most confusing entries in JD2 by searching against the .txt we will generate once you type a name and press ENTER: ")
+
+with open (filename, "w+") as file:
+	file.write("\n        \n")
+url = "https://www.reddit.com/r/PkgLinks.json?limit=100&after="
+r = requests.get(url, headers = {'User-agent': 'Chrome'})
+thejson = r.json()
+for x in range(101):
+	entry = str(thejson["data"]["children"][x]["data"]["selftext"])
+	title = str(thejson["data"]["children"][x]["data"]["title"])
+	b64only = re.findall(r'((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==))',entry)
+	btostr = str(b64only)
+	bstr2byte = btostr.encode('ascii')
+	decodebyte = base64.b64decode(bstr2byte)
+	link = decodebyte.decode('ascii')
+	with open (filename, "r+") as file:
+		file.write(title)
+		file.write("\n")
+		file.write(entry)
+		file.write("\n")
+		file.write(link)
+		file.write("\n")
+	print(title)
+	print(entry)
+	print(link)
